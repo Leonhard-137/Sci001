2a5940f7c866bcde97b96fc423c3de54c805ddc1
leonhard-137 <xiaozk@ihep.ac.cn>
2025-11-02 22:19:00 +0800

    spec_decom

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ import numpy as np
 import astropy.units as u
 from dust_extinction.parameter_averages import CCM89
 from pyat.mean_rms_spectra import get_mean_rms
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/Mrk142.csv')
 
@@ -13,9 +14,18 @@ ext_model = CCM89(Rv=Rv)
 z = 0.0446
 
 WAVELENGTHS = {
-    'W2': 1928, 'M2': 2246, 'W1': 2600, 'U': 3465,
-    'B': 4392, 'SV': 5468, 'lV': 5383, 'u': 3543, 'g': 4770,
-    'r': 6215, 'i': 7545, 'z': 8700
+    'W2': 1928, 
+    'M2': 2246, 
+    'W1': 2600, 
+    'U': 3465,
+    'B': 4392, 
+    'SV': 5468, 
+    'lV': 5383, 
+    'u': 3543, 
+    'g': 4770,
+    'r': 6215, 
+    'i': 7545, 
+    'z': 8700
 }
 
 # 计算 A_lambda (mag)
@@ -55,5 +65,14 @@ for flt in WAVELENGTHS:
     rms_value = rms_mean.loc[flt, 'RMS_Flux_ext_rest']  # 通过索引访问
     X_lambda_t_temp = (flt_data - flt_data.mean()) / rms_value
     X_lambda_t[flt] = X_lambda_t_temp
+    X_lambda_t_mean = X_lambda_t_temp.mean()
+    X_lambda_t_std = X_lambda_t_temp.std()
+    print(f"Filter: {flt}, Mean of X(t): {X_lambda_t_mean}, Std of X(t): {X_lambda_t_std}")
 
-print(X_lambda_t)
+# plt.figure()
+# for flt, X_data in X_lambda_t.items():
+#     plt.plot(X_data, df.loc[df['Filter'] == flt, 'Flux_ext_rest'], label=flt, marker='o', linestyle='', markersize=1)
+# plt.xlabel(r'$X(t)$')
+# plt.ylabel(r'$Flux_\mathrm{ext,rest}$')
+# plt.legend()
+# plt.show()
